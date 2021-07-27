c206d8529dad81dd8fe34d45f2cd404962a15c9e
Bald-M <71381043+Bald-M@users.noreply.github.com>
2021-07-27 16:45:50 +0800

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export PATH
+rm -rf /usr/bin/python3
+rm -rf /usr/bin/pip3
+rm -rf /usr/local/python3
+rm -rf ~/.pip
+echo "Python3.6.8 is uninstalled successfully!"
