461566b3d172271f9bd9c4233d0fdcc0b9ec64fa
Bald-M <71381043+Bald-M@users.noreply.github.com>
2021-07-27 16:45:08 +0800

    Update install_python3.sh
    安装的时候自动换源

--- install_python3.sh
+++ install_python3.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
+#更换豆瓣源
+mkdir ~/.pip
+mv ./pip.conf ~/.pip
+#安装依赖包
 rpm -ivh ./py3/* --force --nodeps
+#源码安装python
 tar -xvf ./Python-3.6.8.tgz -C /usr/local/
 mkdir -p /usr/local/python3
 cd /usr/local/Python-3.6.8/
 ./configure --prefix=/usr/local/python3 && make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
+echo "Python3.6.8 is installed successfully,enjoy!"
